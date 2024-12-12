a811914d3e7c49498dcb82825148c0c75e00ba55
bilhood <bilhood@publicisgroupe.net>
2024-12-12 15:19:26 -0500

    group, member

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "googleworkspace_group" "tfesa" {
+resource "googleworkspace_group" "group" {
   email = var.group_email
 }
 
-data "googleworkspace_user" "bill" {
+data "googleworkspace_user" "member" {
   for_each = var.sa_emails
   primary_email = each.key
 }
 
-resource "googleworkspace_group_member" "manager" {
+resource "googleworkspace_group_member" "member" {
   for_each = var.sa_emails
-  group_id = googleworkspace_group.tfesa.id
-  email    = data.googleworkspace_user.bill.primary_email[each.key]
+  group_id = googleworkspace_group.group.id
+  email    = data.googleworkspace_user.member[each.key].primary_email
   role = "MANAGER"
 }
