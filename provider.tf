12c046794bce0a9477410bdb01000ceee4e39d25
bilhood <bilhood@publicisgroupe.net>
2024-12-11 20:46:30 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,25 @@
+#provider "googleworkspace" {
+#  credentials = "/Users/mscott/my-project-c633d7053aab.json"
+#  customer_id = "A01b123xz"
+#  impersonated_user_email = "impersonated@example.com"
+#  oauth_scopes = [
+#    "https://www.googleapis.com/auth/admin.directory.group",
+#    "https://www.googleapis.com/auth/admin.directory.group.member",
+#    "https://www.googleapis.com/auth/admin.directory.user",
+#    "https://www.googleapis.com/auth/admin.directory.userschema",
+#  ]
+#}
+
+# Auth method: Admin roles applied directly to a service account
 provider "googleworkspace" {
-  credentials = "/Users/mscott/my-project-c633d7053aab.json"
+  #GOOGLEWORKSPACE_CLOUD_KEYFILE_JSON
+  #GOOGLE_CREDENTIALS
+  #credentials = "/Users/mscott/my-project-c633d7053aab.json"
   customer_id = "A01b123xz"
   oauth_scopes = [
     "https://www.googleapis.com/auth/admin.directory.group",
     "https://www.googleapis.com/auth/admin.directory.group.member",
     "https://www.googleapis.com/auth/admin.directory.user",
     "https://www.googleapis.com/auth/admin.directory.userschema",
-    # include scopes as needed
   ]
 }
