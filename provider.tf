b3ddefa1c44e3f773c4c73f9916e486560a6911c
bilhood <bilhood@publicisgroupe.net>
2024-12-11 20:22:17 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,8 @@ provider "googleworkspace" {
   credentials = "/Users/mscott/my-project-c633d7053aab.json"
   customer_id = "A01b123xz"
   oauth_scopes = [
+    "https://www.googleapis.com/auth/admin.directory.group",
+    "https://www.googleapis.com/auth/admin.directory.group.member",
     "https://www.googleapis.com/auth/admin.directory.user",
     "https://www.googleapis.com/auth/admin.directory.userschema",
     # include scopes as needed
