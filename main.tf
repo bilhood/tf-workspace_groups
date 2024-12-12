51e6bc12194a717b31de63df5e5f5628c8da9fa6
bilhood <bilhood@publicisgroupe.net>
2024-12-12 14:54:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 resource "googleworkspace_group" "tfesa" {
-  email = "tfesa@example.com"
+  email = "tfesa@siroot.gcp-foundation.com"
 }
 
-resource "googleworkspace_user" "bill" {
-  primary_email = "bill.hood@example.com"
-  password      = "34819d7beeabb9260a5c854bc85b3e44"
-  hash_function = "MD5"
-
-  name {
-    family_name = "Hood"
-    given_name  = "Bill"
-  }
+data "googleworkspace_user" "bill" {
+  primary_email = "bill.hood@siroot.gcp-foundation.com"
 }
 
 resource "googleworkspace_group_member" "manager" {
