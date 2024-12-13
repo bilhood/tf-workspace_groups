bc5e8499bddf47fb1f51850b387322d2db5345ea
bilhood <bilhood@publicisgroupe.net>
2024-12-13 09:27:29 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,7 @@
-variable "member_emails" {
-  description = "list of serviceaccount/user/group member emails"
-  type = set(string)
-  default = ["bill.hood@siroot.gcp-foundation.com","configconnector@prj-se-bilhood-5609.iam.gserviceaccount.com","gkenode@prj-se-bilhood-5609.iam.gserviceaccount.com"]
-}
-
-variable "group_email" {
-  description = "list of service account emails"
-  type = string
-  default = "tfesa@siroot.gcp-foundation.com"
+variable "groups" {
+  description = "map of group email with set of member emails"
+  type = map(set(string))
+  default = {
+    "tfesa@siroot.gcp-foundation.com" = ["bill.hood@siroot.gcp-foundation.com","configconnector@prj-se-bilhood-5609.iam.gserviceaccount.com","gkenode@prj-se-bilhood-5609.iam.gserviceaccount.com"]
+  }
 }
