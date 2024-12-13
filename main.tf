b8652db8e3daa5cd05ec63dd1427be2a038cdf1c
bilhood <bilhood@publicisgroupe.net>
2024-12-13 09:15:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,9 @@ resource "googleworkspace_group" "group" {
   email = var.group_email
 }
 
-data "googleworkspace_user" "member" {
-  for_each = var.sa_emails
-  primary_email = each.key
-}
-
 resource "googleworkspace_group_member" "member" {
-  for_each = var.sa_emails
+  for_each = var.member_emails
   group_id = googleworkspace_group.group.id
-  email    = data.googleworkspace_user.member[each.key].primary_email
+  email    = each.key
   role = "MANAGER"
 }
