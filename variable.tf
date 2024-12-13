74e0d285017298ee44588557ef0a9afdc68b017e
bilhood <bilhood@publicisgroupe.net>
2024-12-13 09:13:35 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,7 +2,12 @@ variable "sa_emails" {
   description = "list of service account emails"
   type = set(string)
   default = ["configconnector@prj-se-bilhood-5609.iam.gserviceaccount.com","gkenode@prj-se-bilhood-5609.iam.gserviceaccount.com"]
-  #default = ["bill.hood@siroot.gcp-foundation.com"]
+}
+
+variable "user_emails" {
+  description = "list of service account emails"
+  type = set(string)
+  default = ["bill.hood@siroot.gcp-foundation.com"]
 }
 
 variable "group_email" {
